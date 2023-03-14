95b025c806cfbdec153661c62287c02974c8f54f
Tim Van Onckelen <tim@xeweb.be>
2023-03-14 17:50:54 +0100

    Added basic compare function with dummy data

--- src/components/company/compare.tsx
+++ src/components/company/compare.tsx
@@ -1,22 +1,67 @@
 import { Button, TextField, Container, Grid } from "@mui/material";
 import React from "react";
 
-const compare = () => {
+interface ICompareResult {
+  [key: string | number]: any;
+}
+
+const Compare = () => {
+  const [compareResult, setCompareResult] = React.useState<ICompareResult[]>(
+    []
+  );
+  const [company1, setCompany1] = React.useState<string>("");
+  const [company2, setCompany2] = React.useState<string>("");
+
+  const onCompare = React.useCallback(() => {
+    const dummyData: ICompareResult[] = [
+      {
+        total: 100,
+        name: company1,
+      },
+      {
+        total: 200,
+        name: company2,
+      },
+    ];
+    return setCompareResult(dummyData);
+  }, [company1, company2, setCompareResult]);
+
   return (
     <Container className="container text-centered">
       <Grid container spacing={2}>
         <Grid xs={5}>
-          <TextField id="company1" label="Bedrijf 1" variant="outlined" />
+          <TextField
+            id="company1"
+            label="Bedrijf 1"
+            variant="outlined"
+            value={company1}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setCompany1(event.target.value);
+            }}
+          />
         </Grid>
         <Grid xs={2}>
-          <Button>Vergelijk</Button>
+          <Button onClick={onCompare}>Vergelijk</Button>
         </Grid>
         <Grid xs={5}>
-          <TextField id="company2" label="Bedrijf 2" variant="outlined" />
+          <TextField
+            id="company2"
+            label="Bedrijf 2"
+            variant="outlined"
+            value={company2}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setCompany2(event.target.value);
+            }}
+          />
         </Grid>
       </Grid>
+      {compareResult?.map((el) => (
+        <div>
+          {el.name} - {el.total}
+        </div>
+      ))}
     </Container>
   );
 };
 
-export default compare;
+export default Compare;

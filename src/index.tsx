6092821d94621974738c1f61474f840bb38c3cfd
Tim Van Onckelen <tim@xeweb.be>
2023-03-23 20:19:06 +0100

    Added docs to router

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import { router } from "./router";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+// Start van de applicatie
+// Router laadt de nodige components vanuit router.tsx
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
